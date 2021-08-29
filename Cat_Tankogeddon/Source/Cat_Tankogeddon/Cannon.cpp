8b5ce7ec297db086d0a25bbe6b728d8b4c2b0231
CatherineGlv <77730750+CatherineGlv@users.noreply.github.com>
2021-08-29 12:45:55 +1000

    Stop on BP_Projectile create

--- Cat_Tankogeddon/Source/Cat_Tankogeddon/Cannon.cpp
+++ Cat_Tankogeddon/Source/Cat_Tankogeddon/Cannon.cpp
@@ -9,6 +9,10 @@
 #include <TimerManager.h>
 #include <Engine/World.h>
 
+#include "Cat_Tankogeddon.h"
+#include "Projectile.h"
+#include <DrawDebugHelpers.h>
+
 // Sets default values
 ACannon::ACannon()
 {
@@ -27,28 +31,50 @@ ACannon::ACannon()
 
 void ACannon::Fire()
 {
-    if (!bReadyToFire)
+    if (!IsReadyToFire())
     {
         return;
     }
 
     bReadyToFire = false;
+    --NumAmmo;
+    ShotsLeft = NumShotsInSeries;
+    Shot();
+
+    UE_LOG(LogTankogeddon, Log, TEXT("Fire! Ammo left: %d"), NumAmmo);
+}
+
+void ACannon::FireSpecial()
+{
+    if (!HasSpecialFire() || !IsReadyToFire())
+    {
+        return;
+    }
+
+    bReadyToFire = false;
+    --NumAmmo;
 
     if (Type == ECannonType::FireProjectile)
     {
-        GEngine->AddOnScreenDebugMessage(10, 1, FColor::Green, "Fire - projectile");
+        GEngine->AddOnScreenDebugMessage(10, 1, FColor::Green, TEXT("Fire special - projectile"));
     }
     else
     {
-        GEngine->AddOnScreenDebugMessage(10, 1, FColor::Green, "Fire - trace");
+        GEngine->AddOnScreenDebugMessage(10, 1, FColor::Green, TEXT("Fire special - trace"));
     }
 
     GetWorld()->GetTimerManager().SetTimer(ReloadTimerHandle, this, &ACannon::Reload, 1.f / FireRate, false);
+    UE_LOG(LogTankogeddon, Log, TEXT("FireSpecial! Ammo left: %d"), NumAmmo);
 }
 
-bool ACannon::IsReadyToFire()
+bool ACannon::IsReadyToFire() const
 {
-    return bReadyToFire;
+    return bReadyToFire && NumAmmo > 0 && ShotsLeft == 0;
+}
+
+bool ACannon::HasSpecialFire() const
+{
+    return bHasSpecialFire;
 }
 
 // Called when the game starts or when spawned
@@ -57,6 +83,8 @@ void ACannon::BeginPlay()
     Super::BeginPlay();
 
     bReadyToFire = true;
+    ShotsLeft = 0;
+    NumAmmo = MaxAmmo;
 }
 
 void ACannon::EndPlay(const EEndPlayReason::Type EndPlayReason)
@@ -64,10 +92,61 @@ void ACannon::EndPlay(const EEndPlayReason::Type EndPlayReason)
     Super::EndPlay(EndPlayReason);
 
     GetWorld()->GetTimerManager().ClearTimer(ReloadTimerHandle);
+    GetWorld()->GetTimerManager().ClearTimer(SeriesTimerHandle);
 }
 
 void ACannon::Reload()
 {
     bReadyToFire = true;
+}
+
+void ACannon::Shot()
+{
+    check(ShotsLeft > 0)
+        if (Type == ECannonType::FireProjectile)
+        {
+            GEngine->AddOnScreenDebugMessage(INDEX_NONE, 1, FColor::Green, TEXT("Fire - projectile"));
+
+            AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, ProjectileSpawnPoint->GetComponentLocation(), ProjectileSpawnPoint->GetComponentRotation());
+            if (Projectile)
+            {
+                Projectile->Start();
+            }
+        }
+        else
+        {
+            GEngine->AddOnScreenDebugMessage(INDEX_NONE, 1, FColor::Green, TEXT("Fire - trace"));
+
+            FHitResult HitResult;
+            FCollisionQueryParams TraceParams = FCollisionQueryParams(FName(TEXT("FireTrace")), true, this);
+            TraceParams.bTraceComplex = true;
+            TraceParams.bReturnPhysicalMaterial = false;
+
+            FVector Start = ProjectileSpawnPoint->GetComponentLocation();
+            FVector End = ProjectileSpawnPoint->GetForwardVector() * FireRange + Start;
+            if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECollisionChannel::ECC_Visibility, TraceParams))
+            {
+                DrawDebugLine(GetWorld(), Start, HitResult.Location, FColor::Red, false, 0.5f, 0, 5);
+                if (HitResult.Actor.Get())
+                {
+                    HitResult.Actor.Get()->Destroy();
+                }
+            }
+            else
+            {
+                DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 0.5f, 0, 5);
+            }
+
+        }
+
+    if (--ShotsLeft > 0)
+    {
+        const float NextShotTime = SeriesLength / (NumShotsInSeries - 1);
+        GetWorld()->GetTimerManager().SetTimer(SeriesTimerHandle, this, &ACannon::Shot, NextShotTime, false);
+    }
+    else
+    {
+        GetWorld()->GetTimerManager().SetTimer(ReloadTimerHandle, this, &ACannon::Reload, 1.f / FireRate, false);
+    }
 }
 
